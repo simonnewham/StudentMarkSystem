aaf43d459050b508c47ee9d7d7d2f6733bfc769b
simonnewham <32126340+simonnewham@users.noreply.github.com>
2017-09-21 10:51:30 +0200

    Add files via upload
    Updated UI

---
+++ src/UI/UI/AdminStaffHomeController.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author simonnewham
+ */
+public class AdminStaffHomeController implements Initializable {
+
+   @FXML
+   private Button signout;
+       
+   @FXML
+   private StackPane content;
+    
+   @FXML
+   private ImageView logo;
+   
+   @FXML
+   public void handleSignOut(ActionEvent event) throws IOException{
+       
+       Parent home_parent = FXMLLoader.load(getClass().getResource("Login.fxml"));
+       Scene home_scene = new Scene(home_parent);
+       Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //current stage
+       app_stage.setScene(home_scene);
+       app_stage.show();  
+   }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+          //load logo
+        File file = new File("logo.gif");
+        Image image = new Image(file.toURI().toString());
+        logo.setImage(image);
+    }    
+    
+}
---
+++ src/UI/UI/LoginController.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import DB.*;
+import Users.*;
+
+
+/**
+ *
+ * @author simonnewham
+ */
+public class LoginController implements Initializable {
+    
+   @FXML
+   private Button login;
+   @FXML
+   private Text error;
+   @FXML
+   private PasswordField passwordField;
+   @FXML
+   private TextField username;
+   @FXML
+   private ImageView logo;
+   
+   @FXML
+   public void handleLogin(ActionEvent event) throws IOException{
+       
+       //check credentials
+       String u = username.getText();
+       String p = passwordField.getText();
+       
+       //access DB
+       //DataBase DB = new DataBase();
+       ArrayList<User> users = DataBase.getUsers();
+       
+       //TRACE
+       /*for(int i=0; i<users.size(); i++){
+            System.out.println(users.get(i).getUser());
+            System.out.println(users.get(i).getPass());
+       }*/
+      if(u.equals("Admin")){
+                Parent home_parent = FXMLLoader.load(getClass().getResource("AdminHome.fxml"));
+                Scene home_scene = new Scene(home_parent);
+                Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //current stage
+                            //app_stage.hide();
+                app_stage.setScene(home_scene);
+                app_stage.show();   
+            }
+      else if(u.equals("AdminStaff")){
+                Parent home_parent = FXMLLoader.load(getClass().getResource("AdminStaffHome.fxml"));
+                Scene home_scene = new Scene(home_parent);
+                 Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //current stage
+                 //app_stage.hide();
+                app_stage.setScene(home_scene);
+                app_stage.show();   
+            }
+       
+       //Check Credentials
+       for (User temp : users) {
+           
+            
+            
+           if (u.equals(temp.getUser()) && p.equals(temp.getPass())){
+                
+               System.out.println("Welcome "+temp.getUser());
+               
+                        //if convenor
+                        if(temp.getRole().equals("CC")){
+                            
+                           Parent home_parent = FXMLLoader.load(getClass().getResource("convenorHome.fxml"));
+                           Scene home_scene = new Scene(home_parent);
+                           Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //current stage
+                       
+                           app_stage.hide();
+                           app_stage.setScene(home_scene);
+                           app_stage.setTitle("CC view");
+                           app_stage.show();
+                        }
+                        //if student
+                        else if(temp.getRole().equals("S")){
+                            
+                            Parent home_parent = FXMLLoader.load(getClass().getResource("Student_view.fxml"));
+                            Scene home_scene = new Scene(home_parent);
+                            Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //current stage
+                            app_stage.hide();
+                            app_stage.setScene(home_scene);
+                            app_stage.show();    
+                        }
+                        else if(temp.getRole().equals("L")){
+                            
+                            Parent home_parent = FXMLLoader.load(getClass().getResource("Lecturer_view.fxml"));
+                            Scene home_scene = new Scene(home_parent);
+                            Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //current stage
+                            app_stage.hide();
+                            app_stage.setScene(home_scene);
+                            app_stage.show();    
+                        }
+                        
+            
+            }
+            else{
+                error.setText("Error with Login");
+            
+            }
+	}
+   }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //load logo
+        File file = new File("logo.gif");
+        Image image = new Image(file.toURI().toString());
+        logo.setImage(image);
+    }    
+    
+}
